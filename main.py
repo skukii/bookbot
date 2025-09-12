0a36a661a3ab0eac6f68a6b5ea924f27eb413c33
skukii <ana.skuks@gmail.com>
2025-09-12 12:56:34 +0200

    no of words

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def main():
+    path_to_file = "books/frankenstein.txt"
+
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        num_words = len(file_contents.split(" "))
+    
+    
+    print(num_words," words found in the document")
+
+main()
