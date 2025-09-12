87cf2f4868ff3298545961e61429355cf5de3df3
skukii <ana.skuks@gmail.com>
2025-09-12 16:49:34 +0200

    no of words

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from stats import get_num_words
+
 def main():
     path_to_file = "books/frankenstein.txt"
 
     with open(path_to_file) as f:
         file_contents = f.read()
-        num_words = len(file_contents.split(" "))
+        num_words = get_num_words(file_contents)
     
     
     print(num_words," words found in the document")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(s):
+    return len(s.split(" "))
+    
