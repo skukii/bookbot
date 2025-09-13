93acc165929154ce82ad7fd621ba88be642d3698
skukii <ana.skuks@gmail.com>
2025-09-13 10:58:54 +0200

    adding stats

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from stats import get_num_words
+from stats import get_num_words, mapping_chars
 
 def main():
     path_to_file = "books/frankenstein.txt"
 
     with open(path_to_file) as f:
         file_contents = f.read()
-        num_words = get_num_words(file_contents)
+    
+    num_words = get_num_words(file_contents)
+    chars = mapping_chars(file_contents)
     
     print(num_words," words found in the document")
+    print(chars)
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(s):
     return len(s.split())
     
+def mapping_chars(s):
+    helper = s.lower()
+    d = {}
+
+    for k in s:
+        if k not in d.values():
+            d[k] = 1
+        else:
+            h = d.get(k)
+            d.update({k: h+1})
+    return d
